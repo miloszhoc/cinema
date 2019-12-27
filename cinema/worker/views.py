934219d54161d93c79dfcc0c973f23e0cc0b3694
milosz.hoc <miloszhoc@gmail.com>
2019-12-27 23:58:47 +0100

    update worker views

--- cinema/worker/views.py
+++ cinema/worker/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.db import connection
+
 
 # Create your views here.
+def panel(request):
+    return render(request, 'worker/panel.html', context={})
