fa383b046fd1141d3dd5a986a30e07274f2acf96
milosz.hoc <miloszhoc@gmail.com>
2019-12-27 23:56:36 +0100

    updated client views

--- cinema/client/views.py
+++ cinema/client/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from worker.models import *
 
-# Create your views here.
+
+class IndexMovieListView(ListView):
+    template_name = 'client/index.html'
+    model = Movie
+    queryset = Movie.objects.all()
+
+
+class FilmMovieDetailView(DetailView):
+    template_name = 'client/film.html'
+    model = Movie
+
+    def get_context_data(self, **kwargs):
+        context = super(FilmMovieDetailView, self).get_context_data(**kwargs)
+        context['showtime'] = Showtime.objects.filter(movie_id=self.get_object())
+        return context
+
+
+def index(request):
+    return render(request, 'client/index.html', context={})
+
+
+def cennik(request):
+    return render(request, 'client/cennik.html', context={})
+
+
+def kontakt(request):
+    return render(request, 'client/kontakt.html', context={})
+
+
+def okinie(request):
+    return render(request, 'client/okinie.html', context={})
+
+
+def repertuar(request):
+    return render(request, 'client/repertuar.html', context={})
+
+
+def rezerwacja(request):
+    return render(request, 'client/rezerwacja.html', context={})
