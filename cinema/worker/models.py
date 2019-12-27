ff504d7db11910c5f34bb4e8121a52d85028c40f
milosz.hoc <miloszhoc@gmail.com>
2019-12-27 23:59:02 +0100

    update database models

--- cinema/worker/models.py
+++ cinema/worker/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 
-class Clients(models.Model):
+class Client(models.Model):
     client_id = models.AutoField(primary_key=True, null=False)
     first_name = models.CharField(max_length=55)
     last_name = models.CharField(max_length=55)
@@ -16,21 +16,22 @@ class Clients(models.Model):
         return reverse('worker:worker-client-details', kwargs={'client_id': self.client_id})
 
 
-class Movies(models.Model):
+class Movie(models.Model):
     movie_id = models.AutoField(primary_key=True, null=False)
-    movie_title = models.CharField(max_length=255)
-    movie_director = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
     release_date = models.IntegerField()
     duration = models.DurationField(null=False)
     description = models.TextField(null=True)
     link = models.CharField(max_length=255, null=True)
     thumbnail = models.CharField(max_length=255, null=True)
+    trailer_youtube_id = models.CharField(max_length=255, null=True)
 
     def __str__(self):
-        return self.movie_title
+        return str(self.movie_id) + '. ' + self.title
 
 
-class Seats(models.Model):
+class Seat(models.Model):
     seat_id = models.AutoField(primary_key=True)
     seat_number = models.IntegerField(null=True)
 
@@ -40,16 +41,17 @@ class Seats(models.Model):
 
 class Showtime(models.Model):
     showtime_id = models.AutoField(primary_key=True, null=False)
-    movie_id = models.ForeignKey(Movies, on_delete=models.PROTECT)
-    start_time = models.DateTimeField()
+    movie_id = models.ForeignKey(Movie, on_delete=models.PROTECT)
+    start_date = models.DateField(null=True)
+    start_time = models.TimeField(null=True)
     show_break = models.DurationField(default=0)
     end_time = models.DateTimeField(null=True)
 
     def __str__(self):
-        return str(self.showtime_id)
+        return str(self.movie_id.title) + ' - ' + str(self.start_date) + ' ' + str(self.start_time)
 
 
-class TicketTypes(models.Model):
+class TicketType(models.Model):
     ticket_id = models.AutoField(primary_key=True, null=False)
     type = models.CharField(max_length=40)
     price = models.IntegerField()
@@ -60,16 +62,17 @@ class TicketTypes(models.Model):
 
 class Ticket(models.Model):
     ticket_id = models.AutoField(primary_key=True, null=False)
-    client_id = models.ForeignKey(Clients, on_delete=models.PROTECT)
-    tickettype_id = models.ForeignKey(TicketTypes, on_delete=models.PROTECT)
-    seat_id = models.ForeignKey(Seats, on_delete=models.PROTECT)
+    client_id = models.ForeignKey(Client, on_delete=models.PROTECT)
+    tickettype_id = models.ForeignKey(TicketType, on_delete=models.PROTECT)
+    seat_id = models.ForeignKey(Seat, on_delete=models.PROTECT)
 
 
-class Reservations(models.Model):
+class Reservation(models.Model):
     reservation_id = models.AutoField(primary_key=True, null=False)
     # client_id = models.ForeignKey(Clients, on_delete=models.ValueRange)  # do not delete client data
-    ticket_id = models.ForeignKey(Ticket, on_delete=models.PROTECT)  # do not delete ticket type
+    ticket_id = models.ForeignKey(Ticket, on_delete=models.CASCADE)  # do not delete ticket type
     showtime_id = models.ForeignKey(Showtime, on_delete=models.PROTECT)  # do not delete showtime
+
     # seat_id = models.ForeignKey(Seats, on_delete=models.PROTECT)  # do not delete seat
 
     def __str__(self):
