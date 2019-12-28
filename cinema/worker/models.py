5afeb1097bd2b7982cbc3551d37d42c4decc6b03
milosz.hoc <miloszhoc@gmail.com>
2019-12-28 22:35:05 +0100

    updated models

--- cinema/worker/models.py
+++ cinema/worker/models.py
@@ -45,7 +45,8 @@ class Showtime(models.Model):
     start_date = models.DateField(null=True)
     start_time = models.TimeField(null=True)
     show_break = models.DurationField(default=0)
-    end_time = models.DateTimeField(null=True)
+    end_date = models.DateField(null=True)
+    end_time = models.TimeField(null=True)
 
     def __str__(self):
         return str(self.movie_id.title) + ' - ' + str(self.start_date) + ' ' + str(self.start_time)
@@ -54,7 +55,7 @@ class Showtime(models.Model):
 class TicketType(models.Model):
     ticket_id = models.AutoField(primary_key=True, null=False)
     type = models.CharField(max_length=40)
-    price = models.IntegerField()
+    price = models.FloatField()
 
     def __str__(self):
         return self.type
@@ -65,15 +66,16 @@ class Ticket(models.Model):
     client_id = models.ForeignKey(Client, on_delete=models.PROTECT)
     tickettype_id = models.ForeignKey(TicketType, on_delete=models.PROTECT)
     seat_id = models.ForeignKey(Seat, on_delete=models.PROTECT)
+    showtime_id = models.ForeignKey(Showtime, on_delete=models.PROTECT)
 
 
 class Reservation(models.Model):
     reservation_id = models.AutoField(primary_key=True, null=False)
-    # client_id = models.ForeignKey(Clients, on_delete=models.ValueRange)  # do not delete client data
-    ticket_id = models.ForeignKey(Ticket, on_delete=models.CASCADE)  # do not delete ticket type
-    showtime_id = models.ForeignKey(Showtime, on_delete=models.PROTECT)  # do not delete showtime
-
-    # seat_id = models.ForeignKey(Seats, on_delete=models.PROTECT)  # do not delete seat
+    client_id = models.ForeignKey(Client, on_delete=models.PROTECT)
+    showtime_id = models.ForeignKey(Showtime, on_delete=models.PROTECT)
+    cost = models.FloatField(null=True)
+    is_paid = models.BooleanField(default=False)
 
     def __str__(self):
-        return str(self.reservation_id)
+        return str(self.reservation_id) + ' - ' + str(self.client_id.first_name) + ' ' + str(
+            self.client_id.last_name) + ' - ' + str(self.showtime_id.movie_id.title)
