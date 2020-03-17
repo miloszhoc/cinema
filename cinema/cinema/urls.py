9b116d380b57d155540b4174cc138dd68d0b22d9
milosz.hoc <miloszhoc@gmail.com>
2020-03-17 23:37:56 +0100

    main site url modification

--- cinema/cinema/urls.py
+++ cinema/cinema/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('worker/', include('worker.urls')),
-    path('client/', include('client.urls')),
+    path('', include('client.urls')),
 ]
