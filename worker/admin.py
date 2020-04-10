f36d64243e4737ad467795e88ed5afb37af07b23
milosz.hoc <miloszhoc@gmail.com>
2020-04-11 00:22:36 +0200

    unregistered groups

--- worker/admin.py
+++ worker/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import Group
 
-admin.site.site_header = 'Zarządzanie grupami i użytkownikami'
+admin.site.site_header = 'Zarządzanie użytkownikami'
 admin.site.site_title = 'Grupy i użytkownicy'
 # admin.site.register(Reservation)
 # admin.site.register(Movie)
@@ -11,3 +12,4 @@ admin.site.site_title = 'Grupy i użytkownicy'
 # admin.site.register(Seat)
 # admin.site.register(Ticket)
 # admin.site.register(Event)
+admin.site.unregister(Group)
