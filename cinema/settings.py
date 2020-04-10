8a8135e72ab207ac85bab7cc2f25f2033d3d360d
milosz.hoc <miloszhoc@gmail.com>
2020-04-11 00:26:54 +0200

    fix for model unregister

--- cinema/settings.py
+++ cinema/settings.py
@@ -29,10 +29,10 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.auth',
     'client.apps.ClientConfig',
     'worker.apps.WorkerConfig',
     'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
