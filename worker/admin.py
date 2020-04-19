28de68180d457d7e80240377ddac7818c599515f
milosz.hoc <miloszhoc@gmail.com>
2020-04-20 00:16:32 +0200

    updated filters

--- worker/admin.py
+++ worker/admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from .models import *
 from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
 
 admin.site.site_header = 'Zarządzanie użytkownikami'
-admin.site.site_title = 'Grupy i użytkownicy'
-# admin.site.register(Reservation)
-# admin.site.register(Movie)
-# admin.site.register(TicketType)
-# admin.site.register(Client)
-# admin.site.register(Showtime)
-# admin.site.register(Seat)
-# admin.site.register(Ticket)
-# admin.site.register(Event)
+admin.site.site_title = 'Użytkownicy'
 admin.site.unregister(Group)
+UserAdmin.list_filter = ('is_staff', 'is_active', 'groups')
